9db7bee77ca2c3d12b14195e5178ecaa2e59ac88
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2020-06-09 11:56:06 +0900

    DB修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,5 @@
 <?php
-/* データベース接続*/ 
-// try{
-//   $db = new PDO('mysql:dbname=kakeibo1;localhost=127.0.0.1; charset=utf8','root','root');
-// }catch(PDOException $e){
-//   print('DB接続エラー:' . $e->getMessage());
-// }
+
 function dbConnect(){
   $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['dbname'] = ltrim($db['path'], '/');
